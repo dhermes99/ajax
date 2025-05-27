17713c4bcee8f83c04f5271255aaab2bf0100fa7
dhermes99 <viniciushermes99@gmail.com>
2025-05-27 13:22:15 +0100

    changing to fetch api

--- main.js
+++ main.js
@@ -16,15 +16,29 @@
 $(document).ready(function () {
   $("#btn-buscar-cep").click(function () {
     const cep = $("#cep").val();
-    const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+    const endpoint = `https://viacep.com.br/ws/${cep}/json/resultado`;
 
-    $.ajax(endpoint).done(function (resposta) {
-      const logradouro = resposta.logradouro;
-      const bairro = resposta.bairro;
-      const cidade = resposta.localidade;
-      const estado = resposta.uf;
-      const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-      $("#endereco").val(endereco);
-    });
+    //     $.ajax(endpoint).done(function (resposta) {
+    //       const logradouro = resposta.logradouro;
+    //       const bairro = resposta.bairro;
+    //       const cidade = resposta.localidade;
+    //       const estado = resposta.uf;
+    //       const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+    //       $("#endereco").val(endereco);
+    //     });
+
+    // WITH FETCH API
+    fetch(endpoint)
+      .then(function (resposta) {
+        return resposta.json();
+      })
+      .then(function (json) {
+        const logradouro = json.logradouro;
+        const bairro = json.bairro;
+        const cidade = json.localidade;
+        const estado = json.uf;
+        const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        $("#endereco").val(endereco);
+      });
   });
 });
